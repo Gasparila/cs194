42e3f16fc957ef41c6fa016776ad865e9fe89732
dshakeri <dshakeri91@gmail.com>
2015-02-10 14:26:08 -0800

    index now returns the pdf itself

--- payroll/payroll_app/views.py
+++ payroll/payroll_app/views.py
@@ -4,34 +4,43 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.hashers import check_password
+from tempfile import *
+from subprocess import Popen, PIPE
 from subprocess import call
 import datetime
 import json
+import reportlab
 from payroll_app.models import Employer, Employee, Job, BonusPay, PayPeriod
 
 
 # Create your views here.
+@csrf_exempt
 def index(request):
+
+    #Employer.objects.all().delete()
+    #Employee.objects.all().delete()
+    #Job.objects.all().delete()
+    #PayPeriod.objects.all().delete()
     # employer1 = Employer(employer_id="12468",employer_name="Amazon", address="265 Lytton Ave.", pay_start=datetime.datetime(2013, 12, 11, 17, 15, 30), pay_end=datetime.datetime(2013, 12, 25, 17, 18, 40), hash_key="HASHVALUE1")
-    # employer2 = Employer(employer_id="13492",employer_name="Microsoft", address="610 Mayfield Ave", pay_start=datetime.datetime(2013, 12, 11, 17, 15, 30), pay_end=datetime.datetime(2013, 12, 25, 17, 18, 40), hash_key="HASHVALUE2")
+    #employer2 = Employer(employer_id="13492",employer_name="Microsoft", address="610 Mayfield Ave", pay_start=datetime.datetime(2013, 12, 11, 17, 15, 30), pay_end=datetime.datetime(2013, 12, 25, 17, 18, 40), hash_key="HASHVALUE2")
     # employee1 = Employee(employee_id="78932",employer_id="12468", employee_name = "Naveen Krishnamurthi", vacation_hours = 6.00, vacation_pay_rate = 60.00, sick_hours = 12.00, sick_pay_rate = 60.00, vacation_accrual_rate = 0.01, address="12795 Calle De La Siena, San Diego CA, 92130")
-    # employee2 = Employee(employee_id="78777",employer_id="13492", employee_name = "Danial Shakeri", vacation_hours = 8.00, vacation_pay_rate = 62.00, sick_hours = 13.00, sick_pay_rate = 64.00, vacation_accrual_rate = 0.02, address="265 Westfield Ave., San Diego CA, 92130")
+    #employee2 = Employee(employee_id="78777",employer_id="13492", employee_name = "Danial Shakeri", vacation_hours = 8.00, vacation_pay_rate = 62.00, sick_hours = 13.00, sick_pay_rate = 64.00, vacation_accrual_rate = 0.02, address="265 Westfield Ave., San Diego CA, 92130")
     # employee3 = Employee(employee_id="78412",employer_id="13492", employee_name = "Kevin Miller", vacation_hours = 10.00, vacation_pay_rate = 65.00,  sick_hours = 14.00, sick_pay_rate = 65.00, vacation_accrual_rate = 0.01, address="710 New York St., New York NY, 94099")
     # job1 = Job(job_id="8933", employee_id="78932", base_rate = 65.00, incremental_hours_1=2.50, incremental_hours_2=0.00, job_title = "Designer")
     # job2 = Job(job_id="7412", employee_id="78932", base_rate = 70.00, incremental_hours_1=1.00, incremental_hours_2=0.00, job_title = "Engineer I")
-    # job3 = Job(job_id="8935", employee_id="78777", base_rate = 68.00, incremental_hours_1=2.50, incremental_hours_2=2.00, job_title = "Engineer II")
-    # job4 = Job(job_id="8936", employee_id="78412", base_rate = 67.00, incremental_hours_1=2.50, incremental_hours_2=0.00, job_title = "Engineer II")
+    #job3 = Job(job_id="8935", employee_id="78777", base_rate = 68.00, incremental_hours_1=2.50, incremental_hours_2=2.00, job_title = "Engineer II")
+    #job4 = Job(job_id="8936", employee_id="78412", base_rate = 67.00, incremental_hours_1=2.50, incremental_hours_2=0.00, job_title = "Engineer II")
     # bp1 = BonusPay(bonus_id="1234", employee_id="78777", amount = 1000.01, pay_start=datetime.datetime(2013, 12, 11, 17, 15, 30), pay_end=datetime.datetime(2013, 12, 25, 17, 18, 40), date_given=datetime.datetime(2013, 12, 25, 17, 18, 40))
     # bp2 = BonusPay(bonus_id="1256", employee_id="78412", amount = 1700.99, pay_start=datetime.datetime(2013, 12, 11, 17, 15, 30), pay_end=datetime.datetime(2013, 12, 25, 17, 18, 40), date_given=datetime.datetime(2013, 12, 25, 17, 18, 40))
     # employer1.save()
-    # employer2.save()
+    #employer2.save()
     # employee1.save()
-    # employee2.save()
+    #employee2.save()
     # employee3.save()
     # job1.save()
     # job2.save()
-    # job3.save()
-    # job4.save()
+    #job3.save()
+    #job4.save()
     # bp1.save()
     # bp2.save()
     #PayPeriod.objects.all().delete()
@@ -52,6 +61,7 @@ def index(request):
     except KeyError:
         raise Http404("Employee info not found")
     tex_name =  "./" + employer_id + "_" + employee_id + ".tex" 
+    pdf_name = employer_id + "_" + employee_id + ".pdf"
     tex = open( tex_name,'w')
     tex_file = "\\documentclass[14pt]{article}\n\\newcommand{\\tab}[1]{\\hspace{.2\\textwidth}\\rlap{1}}\n\\begin{document}\n\\setlength{\\parindent}{0pt}\n\n"
     tex.write(tex_file)
@@ -120,7 +130,11 @@ def index(request):
         tex.write("\\end{document}")
         tex.close();
         call("pdflatex -output-directory payroll_app/static/pdf " + tex_name, shell=True)
-        return render(request, 'index.html', {'payperiods' : payperiod1, 'employer' : employer, 'employee' : employee, 'jobs' : jobs, 'text' : tex_file , 'pdfPath' : "pdf/tests.pdf"})
+        f = open("payroll_app/static/pdf/" + pdf_name , 'r')
+        pdf_contents = f.read()
+        f.close()
+        return HttpResponse(pdf_contents, content_type='application/pdf')
+        #return render(request, 'index.html', {'pdfPath' : "pdf/tests.pdf"})
     
     tex.close()
     return render(request, 'index.html', {})
