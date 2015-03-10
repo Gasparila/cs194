167f7fb17bdfad815f1641ec0a0c4bd17a55ba50
naveen401 <naveen@rescomp-14-278068.stanford.edu>
2015-03-10 00:45:54 -0700

    Fixed all major issues with deployments

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn payroll.wsgi --log-file -
---
+++ payroll/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/naveen/Documents/cs194/payroll/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
