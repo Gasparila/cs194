858024d78777e82c2456b4866c07c3132d9675dc
Gasparila <kmiller4@stanford.edu>
2015-03-17 22:01:40 -0700

    Updated urls due to modified view

--- payroll/payroll/urls.py
+++ payroll/payroll/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('payroll_app.views',
     # Examples:
     url(r'^$', 'index'),    
-    url(r'^addCompany$', 'addCompany'),
     url(r'^addEmployee$', 'addEmployee'),
     url(r'^addJob$', 'addJob'),
     url(r'^submitTimecard$', 'addTimecardData'),
